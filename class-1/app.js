d69c9819cb31a954f0d947bd07b33034d154be7a
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-07 12:56:53 +0500

    first class of javascript, types of variables, data types, arithmetic operators and javascript buit-in functions.

---
+++ class-1/app.js
@@ -0,0 +1,70 @@
+// let variables
+
+// option 1
+
+let firstName = "Bilal";
+
+console.log(firstName);
+
+//option 2
+
+// let lastName;
+// lastName = "Ansari"
+
+// console.log(lastName);
+
+// const variable
+
+// only 1 option
+
+// const num = 12 + 4;
+
+// console.log(num);
+
+// 2 data-types
+// 1st is string that is in between inverted commas eg "Bilal" , "125"
+// 2nd is numbers that is not in between inverted commas eg 12 , 124
+
+// let num1 = 123;
+// let num2 = 989;
+// let sum = num1 + num2;
+
+// console.log(sum);
+
+// arithmetic operators
+// + - * / %
+
+// addition "+" operation
+
+// option 1 (for addition use in numbers)
+
+// let num3 = 12 + 12;
+
+// console.log(num3);
+
+// option 2 (for concatenation use in strings)
+
+// let name1 = "Bilal " + "Ansari";
+
+// console.log(name1);
+
+// javascript built-in functions
+// 1st console.log()
+// 2nd alert()
+// 3rd prompt()
+
+// 2nd function
+
+// alert("Welcome!");
+
+// alert("how are you?");
+
+// 3rd function 
+
+// prompt("What is your name?"); // this prompt() data is not saved/reviewed
+
+// to save/review the data of prompt() we make it "let" variable 
+
+// let fullName = prompt("What is your name?");
+
+// console.log("Your name is " + fullName);
