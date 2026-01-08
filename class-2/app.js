5ac64c4efe5c93ec564d8597c3543cf69105562b
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-08 15:42:08 +0500

    understanding the concepts of prompt, increamentation & decreamentation, array

---
+++ class-2/app.js
@@ -0,0 +1,53 @@
+// let firstName = "Bilal";
+
+// console.log(firstName);
+
+// let userInput = prompt("Enter a number!");
+
+// console.log(userInput + " x " + 1 + " = " + userInput * 1);
+// console.log(userInput + " x " + 2 + " = " + userInput * 2);
+// console.log(userInput + " x " + 3 + " = " + userInput * 3);
+// console.log(userInput + " x " + 4 + " = " + userInput * 4);
+// console.log(userInput + " x " + 5 + " = " + userInput * 5);
+// console.log(userInput + " x " + 6 + " = " + userInput * 6);
+// console.log(userInput + " x " + 7 + " = " + userInput * 7);
+// console.log(userInput + " x " + 8 + " = " + userInput * 8);
+// console.log(userInput + " x " + 9 + " = " + userInput * 9);
+// console.log(userInput + " x " + 10 + " = " + userInput * 10);
+
+// let initialNumber = 0;
+// let userInput = prompt("Enter a number!");
+
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+// console.log(userInput + " x " + ++initialNumber + " = " + userInput * initialNumber);
+
+
+// let num1 = 4;
+// let num2 = 4;
+
+// console.log(num1++); // post-incrementation
+// console.log(++num2); // pre-incrementation
+// console.log(num1--); // post-decrementation
+// console.log(--num2); //pre-decrementaion
+
+// let num1 = 4;
+// let num2 = 5;
+
+// console.log(num1++ * ++num2 + num2 + ++num1);
+
+
+// array data type that is covered by [ ]
+
+// let fruits = ["Mango", "Banana", "Apple", "Peach"];
+// let spckFruit = fruits[1] = "Papaya";
+// let addFruit = fruits[4] = "Strawberry";
+
+// console.log(fruits);
