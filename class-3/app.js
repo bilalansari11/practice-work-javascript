8b93b802b44a3df6703178a66dc73e2f2b53c7a0
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-09 20:58:53 +0500

    concept of DOM & object

--- class-3/app.js
+++ class-3/app.js
@@ -2,10 +2,10 @@ let firstName = "Bilal";
 
 document.write("Bilal");
 
-let Bilal = {
-    lastName : "Ansari",
-    age : 24,
-    gender : "Male"
-}
+// let Bilal = {
+//     lastName : "Ansari",
+//     age : 24,
+//     gender : "Male"
+// }
 
-console.log(Bilal);
+// console.log(Bilal);
