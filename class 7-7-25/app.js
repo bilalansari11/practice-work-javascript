f644d58d7d05b07cd0ce7a18d2bc5eca502d09f6
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-11 18:52:34 +0500

    concepts of comparison-operators with some simple examples

---
+++ class 7-7-25/app.js
@@ -0,0 +1,141 @@
+// comparision operator
+// < less than
+// > greater than
+// <= less than and eaquals to
+// >= greater than and eaquals to
+// === equality operator
+// !== not equals to
+// && and operator
+// || or operator
+
+
+
+
+
+
+
+// let marks = +prompt("enter your marks!");
+
+// if(marks >= 90) {
+//     console.log("you have passed with A+");
+// }
+// else if(marks >= 80) {
+//     console.log("you have passed with A");
+// }
+// else if(marks >= 70) {
+//     console.log("you have passed with B");
+// }
+// else if(marks >= 60) {
+//     console.log("you have passed with C");
+// }
+// else if(marks >= 50) {
+//     console.log("you have passed with D");
+// }
+// else if(marks > 30) {
+//     console.log("you have passed with E");
+// }
+// else {
+//     console.log("you have failed!");
+// }
+
+
+
+
+// let religion = "Islam";
+// let userReligion = prompt("enter your religion!");
+
+// if (userReligion !== religion) {
+//     console.log("you are not allowed!")
+// }
+// else {
+//     console.log("Conguratulations! you get the visa.")
+// }
+
+
+
+
+// let account = 40000;
+// let userAmount = +prompt("Enter an amount");
+
+// if (userAmount <= 40000) {
+//     console.log("Transfer Successful!")
+// }
+// else {
+//     console.log("Insufficient Balance!")
+// }
+
+
+// let userPin = +prompt("Enter your pin!");
+// let pin = 6765;
+
+// if (userPin !== pin){
+//     console.log("Wrong Pin!");
+// }
+// else {
+//     console.log("Correct Pin!")
+// }
+
+
+
+// let shop = "kurkure";
+// let shop1 = "cola";
+
+// if (shop === "kurkure" && shop1 === "cola") {
+//     console.log("le aao");
+// }
+// else {
+//     console.log("mat aao!");
+// }
+
+
+// let shop = "Kurkure";
+// let shop1 = "undefined";
+
+// if (shop === "Kurkure" || shop1 === "Cola") {
+//     console.log("ae mazay!");
+// }
+// else {
+//     console.log("chal be!")
+// }
+
+
+
+// let userMeal = prompt("Enter your food!");
+// let userDrink = prompt("Enter your Drink!");
+// let meal = "Burger";
+// let drink = "Cola";
+
+// if (userMeal === meal && userDrink === drink) {
+//     console.log("Jaldi le aao!");
+// }
+// else {
+//     console.log("Soup le aao!");
+// }
+
+
+
+
+// let userMeal = prompt("Enter your food!");
+// let userDrink = prompt("Enter your Drink!");
+// let meal = "Burger";
+// let drink = "Cola";
+
+// if (userMeal === meal || userDrink === drink) {
+//     console.log("Jaldi le aao!");
+// }
+// else {
+//     console.log("Soup le aao!");
+// }
+
+
+
+
+// let userMobile = prompt("Do you have mobile?");
+// let userLaptop = prompt("Do you have laptop?");
+
+// if (userMobile === "Available" || userLaptop === "Available") {
+//     console.log("you can attempt the quiz");
+// }
+// else {
+//     console.log("you cant attempt the quiz");
+// }
