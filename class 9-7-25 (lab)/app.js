54b13b3ad80cc77a91e3cda3aa44a5f5ccd47b28
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-15 03:52:25 +0500

    concepts of prompt, condotions, dom manipulation & array's methods

---
+++ class 9-7-25 (lab)/app.js
@@ -0,0 +1,86 @@
+// let num1 = +prompt("enter a number");
+// let num2 = +prompt("enter a number");
+
+// let h1 = document.getElementById("text");
+
+// if (num1 === 0 || num2 === 0) {
+//     //console.log("enter a number other then 0!");
+//     h1.innerHTML = "you have entered 0";
+// }
+// else if (num1 > num2) {
+//     //console.log(num1 + " is greater.");
+//     h1.innerHTML = num1 + " is greater.";
+// }
+// else {
+//     //console.log(num2 + " is greater.");
+//     h1.innerHTML = num2 + " is greater."
+// }
+
+
+
+
+
+// declare and initialize three variables and in one variable keep a larger number and then print the larger number in DOM
+
+
+// let num = 12;
+// let num1 = 14;
+// let num2 = 56;
+// let h1 = document.getElementById("text");
+
+// if (num > num1 && num > num2) {
+//     // console.log(num + " is greater.");
+//     h1.innerHTML = num + " is greater.";
+// }
+// else if (num1 > num && num1 > num2) {
+//     // console.log(num1 + " is greater.");
+//     h1.innerHTML = num1 + " is greater.";
+// }
+// else {
+//     // console.log(num2 + " is greater.");
+//     h1.innerHTML = num2 + " is greater.";
+// }
+
+
+
+// let number = 34; create a condition that will check if the number is even and greater than 100 it is, odd and greater 100, less than 100 or equals to 100 12 % 2 = 0 
+
+
+// let num = 104;
+
+// if (num % 2 === 0 && num > 100) {
+//     console.log(num + " is even and greater than 100.");
+// }
+// else if (num % 2 !== 0 && num > 100) {
+//     console.log(num + " is odd and greater than 100.");
+// }
+// else {
+//     console.log(num + " is less than or equals to 100.");
+// }
+
+
+
+// array
+
+// let fruits = ["apple", "banana", "watermelon", "kiwi"];
+
+// fruits.push("mango");  // add new element to end of an array
+// fruits.unshift("pineapple");  // add new element to start of an array
+
+// fruits.shift();  // removes the first element from an array 
+// fruits.pop();  // remove the last element from an array
+
+// console.log(fruits);
+
+
+
+// let colors = ["black", "white", "yellow", "orange", "blue"];
+// let length = colors.length;
+
+// console.log(length);
+
+
+// let shapes = ["square", "triangle", "circle", "rectangle"];
+// let index = shapes.indexOf("circle");
+
+// console.log(index);
