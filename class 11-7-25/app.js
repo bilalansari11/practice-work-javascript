c004a95c733eecc64f5bd3e8a99053e390ee2893
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-19 23:18:38 +0500

    more methods of array, DOM & conditional statements

---
+++ class 11-7-25/app.js
@@ -0,0 +1,86 @@
+// let fruits = ["Apple", "Banana", "Kiwi", "Pineapple"];
+
+// fruits.splice(2, 2);
+
+// console.log(fruits);
+
+// let fruits = ["Strawberry", "Peach", "Guava"];
+
+//fruits.splice(1, 0, "Apple", "Kiwi");
+
+// fruits.pop();
+
+// console.log(fruits);
+
+// let h1 = document.getElementById("text");
+// h1.innerHTML = fruits;
+
+
+
+// clean cities check how many items in the array and print the items in the DOM
+
+// let cleanCities = ["Karachi", "Hyderabad", "Islamabad", "Lahore"];
+// let h1 = document.getElementById("text");
+// h1.innerHTML = cleanCities.length + " " + cleanCities;
+
+
+// enter you city? city exists is in the array and then print you live in a clean city or you do not live in a clean city
+
+// let userCityName = prompt("Enter your city name?");
+// let cleanCities = ["Hyderabad", "Islamabad", "Karachi", "Lahore"];
+// let h1 = document.getElementById("text");
+
+
+// if (cleanCities.indexOf(userCityName) >= 0) {
+//     h1.innerHTML = "you live in a clean city.";
+//     console.log("you live in clean city.");
+// }
+// else {
+//     h1.innerHTML = "you do not live in a clean city.";
+//     console.log("you do not live in a clean city.");
+// }
+
+
+
+// first and last numbers in the array are equal
+
+// let numbers = [1, 2, 45, 9, 4, 1];
+
+// if (numbers[0] === numbers[numbers.length - 1]) {
+//     console.log("first and last items are equal");
+// }
+// else {
+//     console.log("first and last items are not equal");
+// }
+
+
+
+// let students and check if the length of the user array is 5 and if it is not 5 then print seats are available if not then print seats are full
+
+// let userName = prompt("Enter your name?");
+
+// let students = ["Bilal", "Areeb", "Khalid", "Shabbir"];
+
+// let h1 = document.getElementById("text");
+
+// if (students.length < 5) {
+//     h1.innerHTML = "Seats are available! " + students;
+//     // console.log("Seats are available!", students);
+// }
+// else {
+//     h1.innerHTML = "Seats are full! " + students;
+//     console.log("Seats are not available!", students);
+// }
+
+
+// your password exceeds the limit 6 is the limit
+// password accepted
+
+// let userPassword = prompt("Enter your password!");
+
+// if (userPassword.length <= 6) {
+//     console.log("Password is accepted!");
+// }
+// else {
+//     console.log("Password exceed the limit!");
+// }
