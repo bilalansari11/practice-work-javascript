4e30583052054b077f5e09088808eae5bd7bc329
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-23 03:43:17 +0500

    concepts of nested loops

---
+++ class 16-7-25 (lab)/app.js
@@ -0,0 +1,82 @@
+
+// let firstName =["Zain", "Aizaz", "Hamza", "Huzaifa", "Saad"];
+// let lastName = ["Khan", "Baloch", "Butt", "Jatt", "Memon"];
+
+// for (let i = 0; i < firstName.length; i++) {
+//     for (let j= 0; j < lastName.length; j++) {
+//         console.log(firstName[i] + " " + lastName[j]);
+//     }
+// }
+
+// 5 rounds 
+// 2 glass water
+// 10 pushup
+// 5 squats
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("you have completed " + i + " number of rounds.");
+//     for (let j = 1; j <= 2; j++) {
+//         console.log("you have drank " + j + " glass of water.");
+//         for (let k = 1; k <= 10; k++) {
+//             console.log("you have completed " + k + " number pushups.")    
+//         }
+//         for (let l = 1; l <= 5; l++) {
+//             console.log("you have completed " + l + " number of squats.")
+//         }
+//     }
+// }
+
+
+// print a 3 X 3 square using * star
+// ***
+// ***
+// ***
+
+// for (let i = 1; i <= 3; i++) {
+//     let row = "";
+//     for (let j = 1; j<= 3; j++) {
+//         row = row +"* ";
+//     }
+//     console.log(row,i);
+// }
+
+
+
+// for (let i = 1; i <= 3; i++) {
+//     let row = "";
+//     for(let j = 1; j <= i; j++) {
+//         row = row +"* ";
+//     }
+//     console.log(row);
+// }
+
+
+
+// let firstName = ["Saad", "Huzaifa", "Sameed", "Taha"];
+// let lastName = ["Khan", "Shiekh", "Baloch"];
+
+// for (let i = 0; i < firstName.length; i++) {
+//     for (let j = 0; j < lastName.length; j++) {
+//         console.log(firstName[i] + " " + lastName[j]);
+//     }
+// }
+
+
+// 5 rounds 
+// 2 glass water
+// 10 pushup
+// 5 squats
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("you have completed " + i + " rounds.");
+//     for (let j = 1; j <= 2; j++) {
+//         console.log("you have drank " + j + " glass of waters.");
+//         for (let k = 1; k <= 10; k++){
+//             console.log("you have done " + k + " pushups.");
+//         }
+//         for ( let l = 1; l <= 5; l++) {
+//             console.log("you have done " + l + " squats.");
+//         }
+//     }
+// }
