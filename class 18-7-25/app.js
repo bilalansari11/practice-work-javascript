54f61cfb82f91a38fc46dcbb99dbc74f3d10d926
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-26 14:15:29 +0500

    concepts of string methods & use of flag

---
+++ class 18-7-25/app.js
@@ -0,0 +1,74 @@
+// for print even numbers 50
+
+// for (let i = 0; i <= 50; i++) {
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// splice has 3 perameters first is index num, second is how many items to be remove and third is string to add new value.
+
+// let cities = ["Karachi", "Peshawar", "Lahore", "Queta"];
+
+// cities.splice(1, 1, "Islamabad"); 
+
+// console.log(cities);
+
+
+
+// let cities = ["Karachi", "Peshawar", "Lahore", "Queta", "Hyderabad"];
+
+// let favCities = cities.slice(0, 4);
+
+// console.log(cities, favCities);
+
+
+
+
+
+// let fullName = "Bilal Ansari";
+
+// let toUpperFullName = fullName.toUpperCase();
+
+// let toLowerFullName = fullName.toLowerCase();
+
+// console.log(toUpperFullName, toLowerFullName);
+
+
+
+
+// let userName = prompt("Enter your full name?");
+
+// console.log(userName.toLowerCase());
+
+
+// user prompt city in title case
+
+// let userCity = prompt("Enter your city name?");
+
+// let titleUserCity = userCity[0].toUpperCase() + userCity.slice(1).toLowerCase();
+
+// console.log(titleUserCity);
+
+
+// let cleanCity = ["Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta"];
+// let userCity = prompt("Enter your city?");
+// let isClean = false;
+
+// for (let i = 0; i < cleanCity.length; i++) {
+//     if (userCity.toLowerCase() === cleanCity[i].toLowerCase()){
+//         isClean = true;
+//         break;
+//     }
+// }
+
+// if (isClean) {
+//     console.log("You live in a clean city.");
+// }
+// else {
+//     console.log("You do not live in a clean city.")
+// }
+
+
+
+// Lorem Ipsum Dolor sit amet consectetur adipisicing elit. Nam tempora libero veritatis quaerat sit ex corporis cumque illo, sed saepe aut quam doloribus dolores! Quam quisquam quos asperiores dolorem quasi!
