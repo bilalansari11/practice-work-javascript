0f0d23b35fc428cd4b3910d67d511f83212e7fff
Bilal Ahmed Ansari <ansarii.bilal04@gmail.com>
2026-01-29 00:09:59 +0500

    methods of strings and loop

---
+++ class 21-7-25/app.js
@@ -0,0 +1,24 @@
+// let text = "Lorem Ipsum Dolor sit amet consectetur adipisicing elit. Nam tempora libero veritatis quaerat sit ex corporis cumque illo, sed saepe aut quam doloribus dolores! Quam quisquam quos asperiores dolorem quasi!"
+
+// text = text.toLowerCase();
+// console.log(text);
+
+// let makeUpper = true;
+// let result = "";
+
+// for (let i = 0; i < text.length; i++) {
+//     let char = text[i];
+//     if (makeUpper && char >= "a" && char <= "z") {
+//         result = result + char.toUpperCase();
+//         makeUpper = false;
+//     } 
+//     else {
+//         result = result + char.toLowerCase();
+//     }
+
+//     if (char === " ") {
+//         makeUpper = true;
+//     }
+// }
+
+// console.log(result);
